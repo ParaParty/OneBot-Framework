c661c1060dfe7a7b1ee1ca68da74a84cd98bc7fd
Eric_Lian <git@superexercisebook.com>
2021-08-22 19:34:13 +0800

    fix: 调整默认指令路由设置写法

--- OneBot-CommandRoute/CommandRoute/Services/Implements/DefaultOneBotCommandRouteConfiguration.cs
+++ OneBot-CommandRoute/CommandRoute/Services/Implements/DefaultOneBotCommandRouteConfiguration.cs
@@ -2,10 +2,12 @@
 {
     public class DefaultOneBotCommandRouteConfiguration : IOneBotCommandRouteConfiguration
     {
+        private static readonly string[] CommandPrefixConst = {"！", "!", "/"};
+
         /// <summary>
         /// 默认英文指令前缀
         /// </summary>
-        public string[] CommandPrefix => new[] {"！", "!", "/"};
+        public string[] CommandPrefix => CommandPrefixConst;
 
         /// <summary>
         /// 默认指令匹配大小写不敏感
