bea89e07f6784e824dd8b299e41cd589e5a1db11
Eric_Lian <git@superexercisebook.com>
2022-01-30 15:00:45 +0800

    feat: Tracing Tag

--- OneBot-CommandRoute/CommandRoute/Services/Implements/EventService.cs
+++ OneBot-CommandRoute/CommandRoute/Services/Implements/EventService.cs
@@ -51,7 +51,7 @@ public class EventService : IEventService
     /// 中间件源
     /// </summary>
     private readonly ActivitySource _middlewareActivitySource = new ActivitySource("OneBot.Middleware", Assembly.GetExecutingAssembly().GetName().Version!.ToString());
-    
+
     public EventService(IBotService bot, IServiceProvider serviceProvider, IServiceScopeFactory scopeFactory,
         ILogger<EventService> logger, ICommandService commandService)
     {
@@ -64,30 +64,203 @@ public class EventService : IEventService
 
     public void RegisterEventHandler()
     {
-        _bot.SoraService.Event.OnClientConnect += OnGeneralEvent;
-        _bot.SoraService.Event.OnGroupRequest += OnGeneralEvent;
-        _bot.SoraService.Event.OnFriendRequest += OnGeneralEvent;
-        _bot.SoraService.Event.OnFileUpload += OnGeneralEvent;
-        _bot.SoraService.Event.OnGroupAdminChange += OnGeneralEvent;
-        _bot.SoraService.Event.OnGroupMemberChange += OnGeneralEvent;
-        _bot.SoraService.Event.OnGroupMemberMute += OnGeneralEvent;
-        _bot.SoraService.Event.OnFriendAdd += OnGeneralEvent;
-        _bot.SoraService.Event.OnGroupRecall += OnGeneralEvent;
-        _bot.SoraService.Event.OnFriendRecall += OnGeneralEvent;
-        _bot.SoraService.Event.OnGroupCardUpdate += OnGeneralEvent;
-        _bot.SoraService.Event.OnGroupPoke += OnGeneralEvent;
-        _bot.SoraService.Event.OnLuckyKingEvent += OnGeneralEvent;
-        _bot.SoraService.Event.OnHonorEvent += OnGeneralEvent;
-        _bot.SoraService.Event.OnTitleUpdate += OnGeneralEvent;
-        _bot.SoraService.Event.OnOfflineFileEvent += OnGeneralEvent;
-        _bot.SoraService.Event.OnClientStatusChangeEvent += OnGeneralEvent;
-        _bot.SoraService.Event.OnEssenceChange += OnGeneralEvent;
+        _bot.SoraService.Event.OnClientConnect += (sender, e) =>
+        {
+            return OnGeneralEvent(sender, e, activity =>
+            {
+                activity.AddTag("onebot.client_type", e.ClientType)
+                    .AddTag("onebot.client_version_code", e.ClientVersionCode);
+            });
+        };
+        _bot.SoraService.Event.OnGroupRequest += (sender, e) =>
+        {
+            return OnGeneralEvent(sender, e, activity =>
+            {
+                activity.AddTag("onebot.comment", e.Comment)
+                    .AddTag("onebot.sender_id", e.Sender.Id)
+                    .AddTag("onebot.source_group_id", e.SourceGroup.Id)
+                    .AddTag("onebot.sub_type", e.SubType);
+            });
+        };
+        _bot.SoraService.Event.OnFriendRequest += (sender, e) =>
+        {
+            return OnGeneralEvent(sender, e, activity =>
+            {
+                activity.AddTag("onebot.comment", e.Comment)
+                    .AddTag("onebot.sender_id", e.Sender.Id);
+            });
+        };
+        _bot.SoraService.Event.OnFileUpload += (sender, e) =>
+        {
+            return OnGeneralEvent(sender, e, activity =>
+            {
+                activity.AddTag("onebot.sender_id", e.Sender.Id)
+                    .AddTag("onebot.source_group_id", e.SourceGroup.Id)
+                    .AddTag("onebot.file_info.file_id", e.FileInfo.FileId)
+                    .AddTag("onebot.file_info.name", e.FileInfo.Name)
+                    .AddTag("onebot.file_info.size", e.FileInfo.Size)
+                    .AddTag("onebot.file_info.bus_id", e.FileInfo.Busid);
+            });
+        };
+        _bot.SoraService.Event.OnGroupAdminChange += (sender, e) =>
+        {
+            return OnGeneralEvent(sender, e, activity =>
+            {
+                activity.AddTag("onebot.sender_id", e.Sender.Id)
+                    .AddTag("onebot.source_group_id", e.SourceGroup.Id)
+                    .AddTag("onebot.sub_type", e.SubType);
+            });
+        };
+        _bot.SoraService.Event.OnGroupMemberChange += (sender, e) =>
+        {
+            return OnGeneralEvent(sender, e, activity =>
+            {
+                activity.AddTag("onebot.operator_id", e.Operator.Id)
+                    .AddTag("onebot.change_user_id", e.ChangedUser.Id)
+                    .AddTag("onebot.source_group_id", e.SourceGroup.Id)
+                    .AddTag("onebot.sub_type", e.SubType);
+            });
+        };
+        _bot.SoraService.Event.OnGroupMemberMute += (sender, e) =>
+        {
+            return OnGeneralEvent(sender, e, activity =>
+            {
+                activity.AddTag("onebot.operator_id", e.Operator.Id)
+                    .AddTag("onebot.user_id", e.User.Id)
+                    .AddTag("onebot.source_group_id", e.SourceGroup.Id);
+            });
+        };
+        _bot.SoraService.Event.OnFriendAdd += (sender, e) =>
+        {
+            return OnGeneralEvent(sender, e, activity =>
+            {
+                activity.AddTag("onebot.new_friend_id", e.NewFriend.Id);
+            });
+        };
+        _bot.SoraService.Event.OnGroupRecall += (sender, e) =>
+        {
+            return OnGeneralEvent(sender, e, activity =>
+            {
+                activity.AddTag("onebot.operator_id", e.Operator.Id)
+                    .AddTag("onebot.message_sender_id", e.MessageSender.Id)
+                    .AddTag("onebot.source_group_id", e.SourceGroup.Id)
+                    .AddTag("onebot.message_id", e.MessageId);
+            });
+        };
+        _bot.SoraService.Event.OnFriendRecall += (sender, e) =>
+        {
+            return OnGeneralEvent(sender, e, activity =>
+            {
+                activity.AddTag("onebot.sender_id", e.Sender.Id)
+                    .AddTag("onebot.message_id", e.MessageId);
+            });
+        };
+        _bot.SoraService.Event.OnGroupCardUpdate += (sender, e) =>
+        {
+            return OnGeneralEvent(sender, e, activity =>
+            {
+                activity.AddTag("onebot.user_id", e.User.Id)
+                    .AddTag("onebot.new_card", e.NewCard)
+                    .AddTag("onebot.old_card", e.OldCard)
+                    .AddTag("onebot.source_group_id", e.SourceGroup.Id);
+            });
+        };
+        _bot.SoraService.Event.OnGroupPoke += (sender, e) =>
+        {
+            return OnGeneralEvent(sender, e, activity =>
+            {
+                activity.AddTag("onebot.send_user_id", e.SendUser.Id)
+                    .AddTag("onebot.target_user_id", e.TargetUser.Id)
+                    .AddTag("onebot.source_group_id", e.SourceGroup.Id);
+            });
+        };
+        _bot.SoraService.Event.OnLuckyKingEvent += (sender, e) =>
+        {
+            return OnGeneralEvent(sender, e, activity =>
+            {
+                activity.AddTag("onebot.send_user_id", e.SendUser.Id)
+                    .AddTag("onebot.target_user_id", e.TargetUser.Id)
+                    .AddTag("onebot.source_group_id", e.SourceGroup.Id);
+            });
+        };
+        _bot.SoraService.Event.OnHonorEvent += (sender, e) =>
+        {
+            return OnGeneralEvent(sender, e, activity =>
+            {
+                activity.AddTag("onebot.honor", e.Honor)
+                    .AddTag("onebot.target_user_id", e.TargetUser.Id)
+                    .AddTag("onebot.source_group_id", e.SourceGroup.Id);
+            });
+        };
+        _bot.SoraService.Event.OnTitleUpdate += (sender, e) =>
+        {
+            return OnGeneralEvent(sender, e, activity =>
+            {
+                activity.AddTag("onebot.new_title", e.NewTitle)
+                    .AddTag("onebot.target_user_id", e.TargetUser.Id);
+            });
+        };
+        _bot.SoraService.Event.OnOfflineFileEvent += (sender, e) =>
+        {
+            return OnGeneralEvent(sender, e, activity =>
+            {
+                activity.AddTag("onebot.sender_id", e.Sender.Id)
+                    .AddTag("onebot.offline_file_info.name", e.OfflineFileInfo.Name)
+                    .AddTag("onebot.offline_file_info.size", e.OfflineFileInfo.Size)
+                    .AddTag("onebot.offline_file_info.url", e.OfflineFileInfo.Url);
+            });
+        };
+        _bot.SoraService.Event.OnClientStatusChangeEvent += (sender, e) =>
+        {
+            return OnGeneralEvent(sender, e, activity =>
+            {
+                activity.AddTag("onebot.online", e.Online);
+            });
+        };
+        _bot.SoraService.Event.OnEssenceChange += (sender, e) =>
+        {
+            return OnGeneralEvent(sender, e, activity =>
+            {
+                activity.AddTag("onebot.operator_id", e.Operator.Id)
+                    .AddTag("onebot.sender_id", e.Sender.Id)
+                    .AddTag("onebot.source_group_id", e.SourceGroup.Id)
+                    .AddTag("onebot.message_id", e.MessageId)
+                    .AddTag("onebot.essence_change_type", e.EssenceChangeType);
+            });
+        };
 
-        _bot.SoraService.Event.OnGroupMessage += OnGeneralEvent;
-        _bot.SoraService.Event.OnPrivateMessage += OnGeneralEvent;
+        _bot.SoraService.Event.OnGroupMessage += (sender, e) => OnGeneralEvent(sender, e, GroupMessageActivityExtensionFactory(e));
+        _bot.SoraService.Event.OnPrivateMessage += (sender, e) => OnGeneralEvent(sender, e, PrivateMessageActivityExtensionFactory(e));
 
-        _bot.SoraService.Event.OnSelfGroupMessage += OnGeneralEvent;
-        _bot.SoraService.Event.OnSelfPrivateMessage += OnGeneralEvent;
+        _bot.SoraService.Event.OnSelfGroupMessage += (sender, e) => OnGeneralEvent(sender, e, GroupMessageActivityExtensionFactory(e));
+        _bot.SoraService.Event.OnSelfPrivateMessage += (sender, e) => OnGeneralEvent(sender, e, PrivateMessageActivityExtensionFactory(e));
+    }
+
+    private Action<Activity> GroupMessageActivityExtensionFactory(GroupMessageEventArgs e)
+    {
+        return activity =>
+        {
+            activity.AddTag("onebot.is_anonymous_message", e.IsAnonymousMessage);
+
+            if (e.Anonymous != null)
+            {
+                activity.AddTag("onebot.anonymous.id", e.Anonymous.Id)
+                    .AddTag("onebot.anonymous.name", e.Anonymous.Name);
+            }
+
+            activity.AddTag("onebot.sender_id", e.Sender.Id)
+                .AddTag("onebot.source_group_id", e.SourceGroup.Id)
+                .AddTag("onebot.message", e.Message.RawText);
+        };
+    }
+
+    private Action<Activity> PrivateMessageActivityExtensionFactory(PrivateMessageEventArgs e)
+    {
+        return activity =>
+        {
+            activity.AddTag("onebot.sender_id", e.Sender.Id)
+                .AddTag("onebot.message", e.Message.RawText);
+        };
     }
 
     /// <summary>
@@ -95,27 +268,31 @@ public class EventService : IEventService
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
+    /// <param name="activityExtension"></param>
     /// <returns></returns>
-    private async ValueTask OnGeneralEvent(object sender, BaseSoraEventArgs e)
+    private async ValueTask OnGeneralEvent(object sender, BaseSoraEventArgs e, Action<Activity> activityExtension)
     {
         using var scope = _scopeFactory.CreateScope();
+
+        var operationName = e.EventName ?? e.GetType().FullName ?? "OneBot-Event";
+        var activity = _eventActivitySource.CreateActivity(operationName, ActivityKind.Server) ?? new Activity(operationName);
+        activity.Start();
+        activity.AddTag("onebot.type", e.EventName);
+        activity.AddTag("onebot.source_type", e.SourceType);
+        activity.AddTag("onebot.login_uid", e.LoginUid);
+        activityExtension(activity);
 
         var ctx = new OneBotContextDefault();
         ctx.SetSoraEventSender(sender);
         ctx.SetSoraEventArgs(e);
         ctx.SoraServiceScope(scope);
-        
-        var operationName = e.EventName ?? e.GetType().FullName ?? "OneBot-Event";
-        var activity = _eventActivitySource.CreateActivity(operationName, ActivityKind.Server) ?? new Activity(operationName);
-        activity.AddTag("onebot:type", e.EventName);
-        activity.Start();
 
         var ctxHolder = scope.ServiceProvider.GetService<IOneBotContextHolder>();
         if (ctxHolder is OneBotContextHolder holder)
         {
             holder.SetOneBotContext(ctx);
         }
-        
+
         var middleware = scope.ServiceProvider.GetServices<IOneBotMiddleware>().ToImmutableArray();
         var count = middleware.Length;
 
@@ -150,7 +327,7 @@ public class EventService : IEventService
         {
             await _commandService.EventOnException(ctx, ex);
         }
-        
+
         activity.Stop();
     }
 }
