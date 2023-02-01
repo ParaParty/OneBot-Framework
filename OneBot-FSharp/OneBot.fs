d5161de2a796f04f79fbda335656a7b6e85426ff
方程 <937697555@qq.com>
2023-02-01 22:28:06 +0800

    fix: segments和createMessage

--- OneBot-FSharp/OneBot.fs
+++ OneBot-FSharp/OneBot.fs
@@ -4,13 +4,14 @@ open OneBot.CommandRoute.Configuration
 open OneBot.Core.Context
 open OneBot.Core.Util
 open OneBot.Core.Model
-
+open OneBot.Core
+open OneBot.FSharp
 
 module FBot =
     type Context = OneBotContext
 
     type FileId = string
-    type UserId = int
+    type UserId = string
 
     type Message = 
         | Complex of Message List
@@ -25,9 +26,8 @@ module FBot =
         | Video of FileId
         | Voice of FileId
 
-    
     type Reaction = 
-        | Message of Message
+        | Response of Message
         | Mute of int
         | WaitFor of string * int * (Context -> Reaction)
         | Reactions of Reaction List
@@ -45,9 +45,32 @@ module FBot =
     
     let bot (rule : Rule) : Unit = 
 
+        let createMessage (msg : Message) : OneBot.Core.Model.Message.SimpleMessage =
+
+            let messageModel (msg : Message) : Message.MessageSegmentRef = 
+                let f = Message.MessageSegment in match msg with
+                | Text x -> f(Segments.Text(x))
+                | Audio x -> f(Segments.Audio(x))
+                | File x -> f(Segments.File(x))
+                | Image x -> f(Segments.Image(x))
+                | Location (la,lo,t,c) -> f(Segments.Location(la,lo,t,c))
+                | Mention u -> f(Segments.Mention(u))
+                | MentionAll -> f(Segments.MentionAll())
+                | Reply (u,s) -> f(Segments.Reply(u,s))
+                | Video x -> f(Segments.Video(x))
+                | Voice x -> f(Segments.Voice(x))
+
+            match msg with
+            | Complex xs -> List.map messageModel xs |> System.Collections.Generic.List |> Message.SimpleMessage
+            | _ -> messageModel msg |> Message.SimpleMessage
+
+
+
+
+
         let callback (handler : Handler) (context : OneBotContext) : Unit = 
             match handler context with 
-                | Reply text -> context.Reply (new Message.SimpleMessage(text)) |> ignore //暂时忽略ActionRespone
+                | Response msg -> context.Reply(createMessage msg) |> ignore //暂时忽略ActionRespone
                 
 
         let rec configure (builder : CommandRouteNodeBuilder) (rule : Rule) : Unit = 
