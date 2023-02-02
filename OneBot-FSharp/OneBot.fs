f500ca2a637df5b68961914e33a5d1391d681030
方程 <937697555@qq.com>
2023-02-02 16:02:46 +0800

    feat: 添加EnableWhen Rule,修改Fallback Rule

--- OneBot-FSharp/OneBot.fs
+++ OneBot-FSharp/OneBot.fs
@@ -1,5 +1,6 @@
 namespace Onebot.FSharp
 
+open Microsoft.FSharp.Core
 open OneBot.CommandRoute.Configuration
 open OneBot.Core.Context
 open OneBot.Core.Util
@@ -26,27 +27,29 @@ module FBot =
         | Video of FileId
         | Voice of FileId
 
-    type Reaction = 
+    type Reaction =
+        | QuickReply of string
         | Response of Message
-        | Mute of int
         | WaitFor of string * int * (Context -> Reaction)
         | Reactions of Reaction List
         | Ignore
 
     type Handler = Context -> Reaction
     type OneBotCallback = delegate of OneBotContext -> unit
-
-    type Rule =     
+    
+    type Condition = Context -> bool
+    
+    type Rule =
         | Group of Rule List
         | SubRule of string * Rule
         | Command of string * Handler
-        | Fallback of Handler
+        | EnableWhen of Condition * Rule 
+        | Fallback of Handler * Rule
 
+    let konst a b = a
     
-    let bot (rule : Rule) : Unit = 
-
+    let deploy (rule : Rule) : Unit = 
         let createMessage (msg : Message) : OneBot.Core.Model.Message.SimpleMessage =
-
             let messageModel (msg : Message) : Message.MessageSegmentRef = 
                 let f = Message.MessageSegment in match msg with
                 | Text x -> f(Segments.Text(x))
@@ -65,23 +68,92 @@ module FBot =
             | _ -> messageModel msg |> Message.SimpleMessage
 
 
-
-
-
-        let callback (handler : Handler) (context : OneBotContext) : Unit = 
-            match handler context with 
-                | Response msg -> context.Reply(createMessage msg) |> ignore //暂时忽略ActionRespone
-                
-
-        let rec configure (builder : CommandRouteNodeBuilder) (rule : Rule) : Unit = 
+        let callback (hdl : Handler) (cond : Condition) (fallback : Handler) (ctx : OneBotContext) : Unit =
+            let response = if cond ctx then hdl ctx else fallback ctx
+            let rec process r = match r with 
+                                | Response msg -> ctx.Reply(createMessage msg) |> ignore //暂时忽略ActionResponse
+                                | Reactions xs -> List.map process xs |> ignore
+                                | Ignore -> ()
+            process response
+        
+        
+        let rec configure (builder : CommandRouteNodeBuilder) fallback condition rule =
             match rule with
-            | Command (pattern,handler) -> builder.Command (pattern, OneBotCallback(callback handler))
-            | SubRule (pattern,rule) -> builder.Group (pattern, fun newBuilder -> configure newBuilder rule) |> ignore
-            | Fallback handler -> builder.Command (OneBotCallback(callback handler)) |> ignore
-            | Group rules -> List.map (configure builder) rules |> ignore
-            ()
-
-
+            | Group rs -> let _ = List.map (configure builder fallback condition) rs
+                          builder.Command(OneBotCallback(callback fallback (konst true) fallback)) |> ignore
+            | SubRule (p,r) -> builder.Group (p, fun b -> configure b fallback condition r) |> ignore
+            | Command (p,h) -> builder.Command (p,OneBotCallback(callback h condition fallback))
+            | EnableWhen (c,r) -> configure builder fallback c r
+            | Fallback (f,r) -> configure builder f condition r
+        
+    
+        let builder = new CommandRouteNodeBuilder()
+        let defaultCondition = konst true
+        let defaultFallback = konst Ignore
+        configure builder defaultFallback defaultCondition rule
+        
         ()
-
+        
+    // 示例：
+    // let exampleBot =
+    //     let always = fun a _ -> a
+    //     let refuse = QuickReply "您已进入+1机器人黑名单"
+    //     let blackList = ["114514"]
+    //     let mutable queryMode = false
+    //     let mutable enabled = true
+    //     EnableWhen ((fun _ -> enabled),
+    //         Fallback (always refuse,
+    //             EnableWhen ((fun ctx -> not (List.contains ctx.Event.Id blackList)),
+    //                 Fallback ((fun _ -> QuickReply "+1"),
+    //                     Group [
+    //                         Command ("open", always (QuickReply "Bot已开启"));
+    //                         Command ("close", fun _ -> (queryMode <- true; QuickReply "确认关闭(Y/N)?"));
+    //                         EnableWhen ((fun _ -> queryMode),
+    //                             Group [
+    //                                 Command ("Y", fun _ -> (QuickReply "已关闭"))
+    //                                 Command ("N", fun _ -> (queryMode <- false; QuickReply "已取消"))
+    //                             ]
+    //                         ) 
+    //                     ]
+    //                 )
+    //             )
+    //         )                
+    //     )
+    //
+    // deploy exampleBot
+    //
+    // 一样的功能但使用let分解功能点，减少嵌套：
+    // let exampleBot2 =
+    //     let always = fun a _ -> a
+    //     let refuse = QuickReply "您已进入+1机器人黑名单"
+    //     let blackList = ["114514"]
+    //     let mutable enabled = true
+    //     let isEnabled = fun _ -> enabled
+    //     let isNotBlocked = fun (ctx : Context) -> not (List.contains ctx.Event.Id blackList)
+    //     let replyPlusOne = fun _ -> QuickReply "+1"
+    //
+    //     let shutdownFeature =
+    //         let mutable queryMode = false
+    //         let isQueryMode = fun _ -> queryMode
+    //         let shutdownQuery =
+    //              Group [
+    //                  Command ("Y", fun _ -> (QuickReply "已关闭"))
+    //                  Command ("N", fun _ -> (queryMode <- false; QuickReply "已取消"))
+    //              ]
+    //         Group [
+    //             Command ("open", always (QuickReply "Bot已开启"));
+    //             Command ("close", fun _ -> (queryMode <- true; QuickReply "确认关闭(Y/N)?"));
+    //             EnableWhen (isQueryMode,shutdownQuery)
+    //         ]
+    //              
+    //     EnableWhen (isEnabled,
+    //         Fallback (always refuse,
+    //             EnableWhen (isNotBlocked,
+    //                 Fallback (replyPlusOne,shutdownFeature)
+    //             )
+    //         )                
+    //     )
+    //
+    // deploy exampleBot2
+        
   
